84f8ae03c1a92973baf3a04a66ca0d4867b4d392
Davide Santangelo <davide.santangelo@gmail.com>
2015-04-29 16:16:11 +0200

    fix

--- spec/emailhunter_spec.rb
+++ spec/emailhunter_spec.rb
@@ -8,28 +8,28 @@ describe EmailHunter do
 
   it 'search API expect status \'success\'' do
   	VCR.use_cassette 'search API expect status \'success\'' do
-    	email = EmailHunter.new('1869730ed6aa74ba016f11304fdd83ce625ad2c5')
+    	email = EmailHunter.new('Your secret API key')
     	expect(email.search('google.com').status).to eq('success')
   	end
   end
 
   it 'search API expect results > 0' do
   	VCR.use_cassette 'search API expect results > 0' do
-    	email = EmailHunter.new('1869730ed6aa74ba016f11304fdd83ce625ad2c5')
+    	email = EmailHunter.new('Your secret API key')
     	expect(email.search('google.com').results).to be > 0
   	end
   end
 
   it 'exists API expect status \'success\'' do
   	VCR.use_cassette 'exists API expect status \'success\'' do
-    	email = EmailHunter.new('1869730ed6aa74ba016f11304fdd83ce625ad2c5')
+    	email = EmailHunter.new('Your secret API key')
     	expect(email.exist('bonjour@firmapi.com').status).to eq('success')
   	end
   end
 
   it 'exists API expect status \'true\'' do
   	VCR.use_cassette 'exists API expect status \'true\'' do
-    	email = EmailHunter.new('1869730ed6aa74ba016f11304fdd83ce625ad2c5')
+    	email = EmailHunter.new('Your secret API key')
     	expect(email.exist('bonjour@firmapi.com').exist).to eq(true)
   	end
   end
